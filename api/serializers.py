19069235ae9b0e0fa29e869d743c77ffa26b0ace
Victor Ryabov <victorwillhard@gmail.com>
2024-12-05 09:14:15 +0300

    feat: enhance FilmSerializer with is_favorited field and add favorite action to FilmViewSet

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from api.models import Film
 
 
-class FilmSerializer(ModelSerializer):
+class FilmSerializer(serializers.ModelSerializer):
+    is_favorited = serializers.SerializerMethodField()
+
+    def get_is_favorited(self, obj):
+        user = self.context["request"].user
+        if user.is_anonymous:
+            return False
+        return obj.favorited_by.filter(id=user.id).exists()
+
     class Meta:
         model = Film
-        fields = ["title", "description", "release_date"]
+        fields = ["title", "description", "release_date", "is_favorited"]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,49 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from api.models import Film
-from api.serializers import FilmSerializer
+from api.models import Film, Favorite
 from api.permissions import IsAdminOrReadOnly
+from api.serializers import FilmSerializer
 
 
 class FilmViewSet(viewsets.ModelViewSet):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
     permission_classes = [IsAdminOrReadOnly]
+
+    @action(
+        detail=True,
+        methods=["POST", "DELETE"],
+        permission_classes=[IsAuthenticated],
+    )
+    def favorite(self, request, pk):
+        film = self.get_object()
+        user = request.user
+        instance = Favorite.objects.filter(film=film, user=user).first()
+
+        if request.method == "POST":
+            return self._add_to_favorites(instance, film, user)
+        return self._remove_from_favorites(instance)
+
+    def _add_to_favorites(self, instance, film, user):
+        if instance:
+            return Response(
+                {"detail": "Фильм уже добавлен в избранное"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        Favorite.objects.create(film=film, user=user)
+        serializer = FilmSerializer(film, context={"request": self.request})
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def _remove_from_favorites(self, instance):
+        if not instance:
+            return Response(
+                {"detail": "Фильм не найден в избранном"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
