580ea1bcce92030f17a9d886b0e34d21562d3fad
Victor Ryabov <victorwillhard@gmail.com>
2024-12-05 06:15:54 +0300

    feat: add permission classes to FilmViewSet for admin access control

--- api/views.py
+++ api/views.py
@@ -2,8 +2,10 @@ from rest_framework import viewsets
 
 from api.models import Film
 from api.serializers import FilmSerializer
+from api.permissions import IsAdminOrReadOnly
 
 
 class FilmViewSet(viewsets.ModelViewSet):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
+    permission_classes = [IsAdminOrReadOnly]
