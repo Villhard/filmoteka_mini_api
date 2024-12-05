8ac56b4d6d230221e49dae811a618f0c5af3c834
Victor Ryabov <victorwillhard@gmail.com>
2024-12-05 05:46:51 +0300

    feat: add FilmViewSet for managing Film instances

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from api.models import Film
+from api.serializers import FilmSerializer
+
+
+class FilmViewSet(viewsets.ModelViewSet):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
