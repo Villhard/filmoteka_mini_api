22902c5402803d7cd8f3a04f53a5661b964316bc
Victor Ryabov <victorwillhard@gmail.com>
2024-12-05 07:09:59 +0300

    feat: update URL patterns to include Djoser authentication endpoints

--- filmoteka/urls.py
+++ filmoteka/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework.authtoken import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("api-auth/", include("rest_framework.urls")),
-    path("api-token-auth/", views.obtain_auth_token),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
 ]
